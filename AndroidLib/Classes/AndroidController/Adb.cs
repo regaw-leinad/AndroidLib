2b12ab0595d20f41e38fdc298ae9b830072394e5
regaw-leinad <SelmerSaxMan@gmail.com>
2014-10-16 17:53:56 -0700

    Fix n00b deadlock code

--- AndroidLib/Classes/AndroidController/Adb.cs
+++ AndroidLib/Classes/AndroidController/Adb.cs
@@ -32,7 +32,7 @@ namespace RegawMOD.Android
     /// </summary>
     public static class Adb
     {
-        private static object _lock = "U is lawked";
+        private static Object _lock = new Object();
         internal const string ADB = "adb";
         internal const string ADB_EXE = "adb.exe";
         internal const string ADB_VERSION = "1.0.31";
